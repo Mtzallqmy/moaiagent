9642e90e176d03a1fa748eeb3a3ddc5dc18aa178
Mtzallqmy <mtzallqmy@gmail.com>
2026-08-28 17:49:16 +0300

    feat: register embedded Python tools with Agent tool registry

--- app/src/main/java/com/agentdroid/AgentDroidApplication.kt
+++ app/src/main/java/com/agentdroid/AgentDroidApplication.kt
@@ -1,7 +1,14 @@
 package com.agentdroid
 
 import android.app.Application
+import com.agentdroid.integration.EmbeddedPythonRuntime
+import com.agentdroid.integration.createPythonRuntimeTools
 
 class AgentDroidApplication : Application() {
-    val container: AppContainer by lazy { AppContainer(this) }
+    val container: AppContainer by lazy {
+        AppContainer(this).also { built ->
+            val python = EmbeddedPythonRuntime(this, built::workspaceRoot)
+            built.toolRegistry.registerAll(createPythonRuntimeTools(python))
+        }
+    }
 }
