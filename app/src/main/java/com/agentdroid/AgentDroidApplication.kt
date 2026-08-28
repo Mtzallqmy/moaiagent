7b0fcb1e7c1f2dbf870d5d90c261171a5c18a7ff
Mtzallqmy <mtzallqmy@gmail.com>
2026-08-28 17:56:38 +0300

    feat: expose verified runtime pack controller

--- app/src/main/java/com/agentdroid/AgentDroidApplication.kt
+++ app/src/main/java/com/agentdroid/AgentDroidApplication.kt
@@ -2,15 +2,18 @@ package com.agentdroid
 
 import android.app.Application
 import com.agentdroid.integration.AppMcpController
+import com.agentdroid.integration.AppRuntimePackController
 import com.agentdroid.integration.EmbeddedPythonRuntime
 import com.agentdroid.integration.createPythonRuntimeTools
 
 class AgentDroidApplication : Application() {
-    val container: AppContainer by lazy {
-        AppContainer(this).also { built ->
-            val python = EmbeddedPythonRuntime(this, built::workspaceRoot)
-            built.toolRegistry.registerAll(createPythonRuntimeTools(python))
-        }
-    }
+    val container: AppContainer by lazy { AppContainer(this) }
+    val pythonRuntime: EmbeddedPythonRuntime by lazy { EmbeddedPythonRuntime(this, container::workspaceRoot) }
     val mcpController: AppMcpController by lazy { AppMcpController(this, container) }
+    val runtimePacks: AppRuntimePackController by lazy { AppRuntimePackController(this, container, pythonRuntime) }
+
+    override fun onCreate() {
+        super.onCreate()
+        container.toolRegistry.registerAll(createPythonRuntimeTools(pythonRuntime))
+    }
 }
